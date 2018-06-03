37ae973a5c8b37af755a318890da6701dd7078b8
Kalarrs Topham <k-dawg-klakr@hotmail.com>
2018-06-03 08:26:52 -0700

    Reduce code, code format

--- aws/mongo/src/models/user.ts
+++ aws/mongo/src/models/user.ts
@@ -1,6 +1,6 @@
-import bcrypt from "bcrypt-nodejs";
-import crypto from "crypto";
-import mongoose from "mongoose";
+import bcrypt from 'bcrypt-nodejs';
+import crypto from 'crypto';
+import mongoose from 'mongoose';
 import {ObjectId} from 'bson';
 
 export type comparePasswordFunction = (candidatePassword: string, cb: (err: any, isMatch: any) => {}) => void;
@@ -34,13 +34,9 @@ export class User {
     });
   };
 
-  gravatar(size: number): string {
-    if (!size) {
-      size = 200;
-    }
-    if (!this.email) {
-      return `https://gravatar.com/avatar/?s=${size}&d=retro`;
-    }
+  gravatar(size: number = 200): string {
+    if (!this.email) return `https://gravatar.com/avatar/?s=${size}&d=retro`;
+
     const md5 = crypto.createHash('md5').update(this.email).digest('hex');
     return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
   };
