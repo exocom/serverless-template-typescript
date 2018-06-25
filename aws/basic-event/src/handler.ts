52c9dd238d104dba8eb46d74f1b91a038cf93a78
Kalarrs Topham <k-dawg-klakr@hotmail.com>
2018-06-24 21:23:43 -0700

    Fix bug with date.

--- aws/basic-event/src/handler.ts
+++ aws/basic-event/src/handler.ts
@@ -7,7 +7,7 @@ const mongoPromise = MongoClient.connect(process.env.MONGODB_URI);
 export const newUsers: ScheduleEventHandler = async () => {
   const client: MongoClient = await mongoPromise;
 
-  const yesterday = moment().subtract('24 hours');
+  const yesterday = moment().subtract(24, 'hours').toDate();
   const newUsers = await client.db().collection('users').count({createdAt: {$gt: yesterday}});
 
   console.log(`${newUsers} new users accounts create in the last 24 hours`);
