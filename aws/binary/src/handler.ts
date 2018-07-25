5f881b9fd9b37e970661e7d49db8195a5d19a224
Kalarrs Topham <k-dawg-klark@hotmail.com>
2018-07-25 09:44:42 -0700

    Binary Example (#3)
    * Adding binary base. Updated pacakge names. Updated ports.
    
    * Woot
    
    * update phoneme and voice
    
    * updated deps. Added iam perms.

---
+++ aws/binary/src/handler.ts
@@ -0,0 +1,32 @@
+import AWS = require('aws-sdk');
+import {LambdaUtil} from '../../libs/lambda-util';
+
+
+const lambdaUtil = new LambdaUtil();
+const polly = new AWS.Polly();
+
+export const postSpeech = async (event, context) => {
+  // 1st check if file exists on s3.
+  // If so steam it
+
+  // Else create file on polly and then stream to s3 and user
+  const {AudioStream} = await polly.synthesizeSpeech({
+    LexiconNames: [
+      'kalarrs'
+    ],
+    OutputFormat: 'mp3',
+    SampleRate: '8000',
+    Text: 'Hey kalarrs! How are you?',
+    TextType: 'text',
+    VoiceId: 'Kimberly'
+  }).promise();
+
+  return lambdaUtil.apiResponseBinary({
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+      'Content-Type': 'audio/mpeg'
+    },
+    body: (AudioStream as Buffer)
+  });
+};
