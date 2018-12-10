0b2e85d39ec7cf404e58093580aac646e3940126
Kalarrs Topham <k-dawg-klakr@hotmail.com>
2018-12-09 22:54:55 -0700

    KAL-32 Added any-to-sqs template.

---
+++ aws/any-to-sqs/webpack.config.js
@@ -0,0 +1,22 @@
+const {join} = require('path');
+const nodeExternals = require('webpack-node-externals');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: process.env.SLS_DEBUG ? 'development' : 'production',
+  entry: {'src/handler': './src/handler.ts'},
+  output: {
+    libraryTarget: 'commonjs',
+    path: join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+  target: 'node',
+  //node: { __dirname: false}, // Uncomment - If you want to access files on lambda via path.
+  resolve: {extensions: ['.ts', '.js']},
+  module: {
+    rules: [{test: /\.ts$/, loader: 'ts-loader'}],
+  },
+  externals: [nodeExternals({modulesDir: join(__dirname, '../node_modules')})],
+  plugins: [],
+  devtool: process.env.SLS_DEBUG ? 'source-map' : false
+};
