2c09d3772ba39647f6fc2e29fde98b48b66704c1
Kalarrs Topham <k-dawg-klakr@hotmail.com>
2019-01-28 15:38:35 -0700

    Updating to latest libs

--- aws/websockets/src/handler.ts
+++ aws/websockets/src/handler.ts
@@ -1,63 +1,10 @@
 import {ApiGatewayManagementApi} from 'aws-sdk';
-import {ApiGatewayUtil} from '@kalarrs/aws-util';
-import {APIGatewayEventRequestContext, APIGatewayProxyResult, AuthResponseContext, Handler} from 'aws-lambda';
-
-export interface APIGatewayProxyEvent<T = APIGatewayEventRequestContext> {
-  body: string | null;
-  headers: { [name: string]: string };
-  multiValueHeaders: { [name: string]: string[] };
-  httpMethod: string;
-  isBase64Encoded: boolean;
-  path: string;
-  pathParameters: { [name: string]: string } | null;
-  queryStringParameters: { [name: string]: string } | null;
-  multiValueQueryStringParameters: { [name: string]: string[] } | null;
-  stageVariables: { [name: string]: string } | null;
-  requestContext: T;
-  resource: string;
-}
-
-export interface APIGatewayProxyHandler<T = APIGatewayProxyEvent>  extends Handler<T, APIGatewayProxyResult> {
-}
-
-// API Gateway "websocket" request context
-export interface APIGatewayWebsocketEventRequestContext {
-  apiId: string;
-  authorizer?: AuthResponseContext | null;
-  connectedAt: number,
-  connectionId: string,
-  domainName: string,
-  error: string,
-  eventType: 'CONNECT' | 'DISCONNECT' | 'DEFAULT' | 'MESSAGE',
-  extendedRequestId: string;
-  identity: {
-    accessKey: string | null;
-    accountId: string | null;
-    caller: string | null;
-    cognitoAuthenticationProvider: string | null;
-    cognitoAuthenticationType: string | null;
-    cognitoIdentityId: string | null;
-    cognitoIdentityPoolId: string | null;
-    sourceIp: string;
-    user: string | null;
-    userAgent: string | null;
-    userArn: string | null;
-  };
-  integrationLatency: string;
-  messageDirection: 'IN' | 'OUT';
-  messageId: string;
-  requestId: string;
-  requestTime: string;
-  requestTimeEpoch: number;
-  routeKey: string,
-  stage: string;
-  status: string;
-}
+import {ApiGatewayUtil, ApiGatewayWebsocketHandler} from '@kalarrs/aws-util';
 
 
 const apiGatewayUtil = new ApiGatewayUtil();
 
-export const handler: APIGatewayProxyHandler<APIGatewayProxyEvent<APIGatewayWebsocketEventRequestContext>> = async (event, context) => {
+export const handler: ApiGatewayWebsocketHandler = async (event, context) => {
   const {eventType, connectionId, routeKey, domainName, stage} = event.requestContext;
 
   console.log(eventType);
